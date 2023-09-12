e055a340a85e54046bd850c86964ec615a060dac
Carla Meringolo <carlameringolo@gmail.com>
2023-09-12 17:32:09 -0300

    third commit

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from AppCoder.views import *
 
 urlpatterns = [
-    path('agrega-curso/<nombre>/<camada>', curso),
+    path('agrega-curso/<nombre>/<camada>/', curso),
     path('lista-cursos/', listar_cursos),
-    path('', inicio),
-    path('cursos/', cursos),
-    path('profesores/', profesores),
-    path('estudiantes/', estudiantes),
-    path('entregables/', entregables)
+    path('', inicio, name='Inicio'),
+    path('cursos/', cursos, name='Cursos'),
+    path('profesores/', profesores, name='Profesores'),
+    path('estudiantes/', estudiantes, name='Estudiantes'),
+    path('entregables/', entregables,name='Entregables'),
 ]
